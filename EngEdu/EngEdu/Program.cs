680e2093a48ad8ad79993de63ab87258df4191a0
janezthh <61030009@kmitl.ac.th>
2019-08-30 16:11:06 +0700

    “2nd year semester 1”

--- EngEdu/EngEdu/Program.cs
+++ EngEdu/EngEdu/Program.cs
@@ -8,6 +8,7 @@ namespace EngEdu
         {
             Console.WriteLine("Year 255x semester 1,I am an Engedu freshman");
             Console.WriteLine("Year 255x semester 2,I learn to program in C sharp");
+            Console.WriteLine("Year 255x semester 1,I have to regrade on some course");
         }
     }
 }
